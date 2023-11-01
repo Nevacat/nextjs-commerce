40dfdae5c182e11e170f6a4ebad5a46df7bb0258
Nevacat <nevacattery217@naver.com>
2023-11-01 17:51:19 +0900

    feat: Notion을 이용한 데이터베이스 연습

---
+++ src/pages/api/add-item.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from '@notionhq/client';
+
+const notion = new Client({
+  auth: 'secret_w8stwzpw6FUvUZhH6BBD5LSUL1QLywDuj0zd3DxawVo',
+});
+
+const databaseId = '74779e6b4a0847b996a41a520479d25e';
+
+async function addItem(name: string) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: [
+          {
+            text: {
+              content: name,
+            },
+          },
+        ],
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(JSON.stringify(error));
+  }
+}
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { name } = req.query;
+  if (name == null) {
+    return res.status(400).json({ message: 'name is required' });
+  }
+  try {
+    await addItem(String(name));
+    res.status(200).json({ message: 'jacket' });
+  } catch (e) {
+    res.status(400).json({ message: 'failed to add' });
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import { useRef, useState, useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('/api/get-items')
+      .then((res) => res.json())
+      .then((data) => setProducts(data.items));
+  }, []);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    if (inputRef.current == null || inputRef.current.value === '') {
+      alert('Please enter a name');
+      return;
+    }
+
+    fetch(`/api/add-item?name=${inputRef.current.value}`)
+      .then((res) => res.json())
+      .then((data) => alert(data.message));
+  };
+
   return (
     <>
       <Head>
@@ -15,100 +34,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <input type="text" ref={inputRef} placeholder="name" />
+        <button onClick={handleClick}>Add Jacket</button>
+        <div>
+          <p>Products List</p>
+          {products &&
+            products.map((item) => (
+              <div key={item}>
+                {JSON.stringify(item)}
+                <br />
+                <br />
+              </div>
+            ))}
         </div>
       </main>
     </>
-  )
+  );
 }
